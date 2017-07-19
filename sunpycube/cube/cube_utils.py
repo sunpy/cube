1093f0706abd6e060d9cf272a4133dfa8d65aa09
abit2 <ankit.baruah1@gmail.com>
2017-07-20 00:37:40 +0530

    adding default fail case

--- sunpycube/cube/cube_utils.py
+++ sunpycube/cube/cube_utils.py
@@ -549,6 +549,9 @@ def get_cube_from_sequence(cubesequence, item):
                     data[i] = cube[item[2::]]
             result = cubesequence._new_instance(
                 data, meta=cubesequence.meta, common_axis=cubesequence.common_axis)
+        elif:
+            result = None
+            raise ValueError("type of item {0}".format(type(item)))
     return result
 
 
